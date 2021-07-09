07d263509c052606bf54e9162a1bbc2cd5640581
mikey-roberts <mikey_roberts@outlook.com>
2021-07-09 11:11:24 +0100

    feature tests added for game outcomes, all strikes, all spares, gutter game and normal hits

--- spec/feature/featFrameSpec.js
+++ spec/feature/featFrameSpec.js
@@ -11,4 +11,30 @@ describe("Game", () => {
     game.createRoll(1)
     expect(game.roll[0]).toEqual(1);
   });
+
+  it("returns 0 if every throw is a gutter ball", () => {
+    for (let i = 0; i < 20; i++) {
+    game.createRoll(0)
+    }
+    expect(game.score).toEqual(0);
+  });
+  it("calculates 40 if every roll is a 4", () => {
+    for (let i = 0; i < 20; i++) {
+    game.createRoll(4)
+    }
+    expect(game.score).toEqual(40);
+  });
+  it('calculates a perfect game with every shot a strike', () => {
+    for (let i = 0; i < 13; i++) {
+    game.createRoll(10)
+    }
+    expect(game.score).toEqual(300);
+  });
+    it('calculates a perfect game with every shot a strike', () => {
+    for (let i = 0; i < 22; i++) {
+    game.createBall(5)
+    }
+    expect(game.score).toEqual(150);
+  });
+  });
  
