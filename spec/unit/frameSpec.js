92c2d6759a2ef281c84b2c42230741fde0c09042
mikey-roberts <mikey_roberts@outlook.com>
2021-07-09 11:12:57 +0100

    checks if throw is a strike, spare or calculates if normal throw

--- spec/unit/frameSpec.js
+++ spec/unit/frameSpec.js
@@ -16,3 +16,26 @@ describe("Frame", () => {
     });
   });
   
+  describe('#scoreCalc()', () => {
+    it('calculates score', () => {
+      frame.createRoll(1);
+      frame.createRoll(1);
+      expect(frame.scoreCalc()).toEqual(2);
+    });
+  });
+  
+  describe('#isSpare()', () => {
+    it('determines whether the frame is a spare', () => {
+      frame.createRoll(6);
+      frame.createRoll(4);
+      expect(frame.isSpare()).toBeTruthy();
+    });
+  });
+  
+  describe('#isStrike()', () => {
+    it('returns truthy when roll is a strike', () => {
+      frame.createRoll(10);
+      expect(frame.isStrike()).toBeTruthy();
+    });
+  });
+  });
